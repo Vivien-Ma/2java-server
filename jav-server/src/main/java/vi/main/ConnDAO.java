560b9726d8e18d3fa6d66e3eedd784446053bfd9
Vivien.ma <vivien.mauriaucourt@supinfo.com>
2024-01-27 18:46:52 +0100

    firs comit

---
+++ jav-server/src/main/java/vi/main/ConnDAO.java
@@ -0,0 +1,75 @@
+package vi.main;
+import java.io.*;
+import java.net.*;
+
+public class ConnDAO {
+    private ServerSocket connection;
+    private boolean isRunning = false;
+
+    public ConnDAO(){
+        try{
+            // créé un socket
+            connection = new ServerSocket(3000);
+            isRunning = true;
+            System.out.println("Server is running. listening on port 3000 ...");;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void acceptconn(){
+        if (isRunning){
+            while (true) {
+                try {
+                    Socket clientSocket = connection.accept();
+                    System.out.println("Client connected" + clientSocket);
+
+                    //logique pour chaque client dans un thread pour chaque client
+                    ClientHandler clientHandler = new ClientHandler(clientSocket);
+                    Thread clientThread = new Thread(clientHandler);
+                    clientThread.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+            isRunning = false;
+            System.out.println("Server connection closed.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("recieved from " + clientSocket.getInetAddress() + message);
+
+                    //logique de reponse pour chaque thread client ici !!
+                    String reponse = "server recieved:" + message;
+
+                    //close the client conn
+                    clientSocket.close();
+                    System.out.println("client disconnected: " + clientSocket.getInetAddress());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ jav-server/src/main/java/vi/main/Main.java
@@ -0,0 +1,8 @@
+package vi.main;
+
+public class Main {
+    public static void main(String[] args) {
+        ConnDAO conn = new ConnDAO();
+        conn.acceptconn();
+    }
+}
